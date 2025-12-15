1429022d05b2b9f27d2b1f38fc001f9c50e17994
Anniballi-Andrea <andreanniballi@gmail.com>
2025-12-15 16:35:04 +0100

    add storeRevie path in movies

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -20,12 +20,12 @@ const show = (req, res) => {
     const id = Number(req.params.id)
 
     const sql = 'SELECT `id`, `title`, `genre`, `release_year`, `director`, `abstract`, `image`FROM `movies` WHERE `id`= ?'
-    const reviewSql = 'SELECT `name`,`text` AS `review`, `vote`, `created_at`FROM `reviews` WHERE `movie_id`= ?'
+    const reviewSql = 'SELECT `id` AS `review_id`, `name`,`text` AS `review`, `vote`, `created_at` FROM `reviews` WHERE `movie_id`= ?'
 
 
     connection.query(sql, [id], (err, results) => {
         if (err) {
-            return res.status(500).json({
+            return res.status(404).json({
                 error: true,
                 message: err.message
             })
@@ -59,6 +59,18 @@ const store = (req, res) => {
     return res.send('it work')
 }
 
+const storeReview = (req, res) => {
+    const id = Number(req.params.id)
+    const { name, text, vote } = req.body
+
+    const sql = 'INSERT INTO reviews (movie_id, name, text, vote) VALUES (?, ?, ?, ?)'
+    console.log(name, text, vote)
+    connection.query(sql, [id, name, text, vote], (err, results) => {
+        if (err) return res.status(500).json({ error: true, message: err.message })
+        res.status(201).json({ message: "review successful posted" })
+    })
+}
+
 const update = (req, res) => {
     return res.send('it work')
 }
@@ -76,6 +88,7 @@ module.exports = {
     index,
     show,
     store,
+    storeReview,
     update,
     modify,
     destroy
--- routes/movies.js
+++ routes/movies.js
@@ -10,6 +10,8 @@ router.get('/:id', movieController.show)
 
 router.post('/', movieController.store)
 
+router.post('/:id/review', movieController.storeReview)
+
 router.put('/:id', movieController.update)
 
 router.patch('/:id', movieController.modify)
